3adc15b48500ba4fa84bac2604385149640f2cf9
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2024-01-04 22:58:59 +0100

    Update app.pcss and +page.svelte files

--- src/routes/book/[book_path]/+page.svelte
+++ src/routes/book/[book_path]/+page.svelte
@@ -7,19 +7,19 @@
 	import ReaderNav from '$lib/components/custom/readerNav.svelte';
 	import { debounce } from '$lib/utils.js';
 	import * as Sheet from '$lib/components/ui/sheet';
-	import { ListBullet } from 'radix-icons-svelte';
+	import { ChevronLeft, ChevronRight } from 'radix-icons-svelte';
 	import { Button } from '$lib/components/ui/button';
 
 	type Flow = 'auto' | 'paginated' | 'scrolled-doc' | 'scrolled' | undefined;
 
 	const { data } = $props();
 	const { path } = data;
-	const flow: Flow = 'paginated';
+	const flow: Flow = 'scrolled';
 
 	// console.log(data);
 	// const book = new Book(convertFileSrc(path), {});
 
-	const book = $state(new Book(convertFileSrc(path), {}));
+	const book = new Book(convertFileSrc(path), {});
 
 	const renditionOpts: RenditionOptions = {
 		flow,
@@ -33,13 +33,41 @@
 	const location = localStorage.getItem(`${path}-location`);
 	let toc = $state<NavItem[]>([]);
 
-	if (location) {
-		book.locations.load(location);
-
-		book.rendition.display();
-	}
 	// let current_page: number = $state(0);
 	// let total_pages: number = $state(0);
+
+	// Setup
+
+	async function epub_setup() {
+		await book.locations.generate(1024); // Example using 1024 characters per page
+		book.loaded.navigation.then((data) => {
+			toc = data.toc;
+		});
+		if (location) {
+			// const loc = book.locations.load(location);
+
+			const loc = JSON.parse(location);
+			rendition.location = loc;
+			await rendition.display(loc.end.href);
+			console.log('loc', loc);
+		} else {
+			await rendition.display();
+		}
+	}
+
+	// On location change
+
+	rendition.on('locationChanged', async (location) => {
+		const cur_location = rendition.currentLocation();
+		// console.log('cur', cur_location);
+		// console.log('loc', rendition.location);
+
+		localStorage.setItem(`${path}-location`, JSON.stringify(cur_location));
+		// console.log(book.locations.save());
+		// current_page = location.start.displayed.page;
+		// total_pages = location.end.displayed.page;
+	});
+
 	const rtl = false;
 	const goLeft = rtl ? () => rendition.next() : () => rendition.prev();
 	const goRight = rtl ? () => rendition.prev() : () => rendition.next();
@@ -59,22 +87,10 @@
 		true
 	);
 
-	book.loaded.navigation.then((data) => {
-		toc = data.toc;
-	});
-	rendition.on('locationChanged', async (location) => {
-		await book.locations.generate(1024); // Example using 1024 characters per page
-
-		localStorage.setItem(`${path}-location`, book.locations.save());
-		// console.log(book.locations.save());
-		// current_page = location.start.displayed.page;
-		// total_pages = location.end.displayed.page;
-	});
-
 	$effect(() => {
-		// (async () => {
-		// 	await rendition.display();
-		// })();
+		(async () => {
+			await epub_setup();
+		})();
 
 		// console.log(document.documentElement.addEventListener);
 
@@ -122,9 +138,23 @@
 
 {#if toc.length > 0}
 	<ReaderNav {book} tableOfContents={toc}>
-		<main class="relative max-h-[100vh] max-w-[100vw]">
-			<section class="flex h-[88lvh] w-[85lwh] justify-center overflow-x-clip" id="area"></section>
-			<div class="flex justify-items-end"></div>
+		<main class="relative max-h-[100vh] max-w-[100vw] overflow-x-clip">
+			<Button
+				on:click={goLeft}
+				variant="secondary"
+				class=" absolute left-4 top-1/3 z-50 px-4 py-14 opacity-25 transition-all duration-500 hover:opacity-100"
+			>
+				<ChevronLeft class="h-6 w-6 " />
+			</Button>
+			<Button
+				on:click={goRight}
+				variant="secondary"
+				class=" absolute right-4 top-1/3 z-50 px-4 py-14 opacity-25 transition-all duration-500 hover:opacity-100"
+			>
+				<ChevronRight class="h-6 w-6" />
+			</Button>
+			<section class="flex h-[89lvh] w-[85lwh] justify-center overflow-x-clip" id="area"></section>
+
 			<!-- <Slider value={[current_page]} max={100} step={1} /> -->
 		</main>
 	</ReaderNav>
