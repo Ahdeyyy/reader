5e225fb38a9541a0c2031dc2d0b50c737fe51a3e
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2024-01-27 14:14:11 +0100

    chore: rewrote the generate image function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,40 +4,49 @@
 use epub::doc::EpubDoc;
 use std::fs;
 use std::io::Write;
+use std::path::Path;
+
+fn normalize_title(title: &String) -> String {
+    return title
+        .replace("<", "")
+        .replace("\"", "")
+        .replace(":", "")
+        .replace(">", "")
+        .replace("?", "")
+        .replace("|", "")
+        .replace("\\", "")
+        .replace("/", "");
+}
 
 #[tauri::command]
 fn generate_image(app_handle: tauri::AppHandle, path: String) -> Result<(), String> {
     //TODO: fix unwraps
+    //
+    let app_dir = app_handle.path_resolver().app_data_dir().unwrap();
+    let cover_folder = format!("{}\\cover", app_dir.to_string_lossy().clone());
+    if !Path::new(cover_folder.as_str()).exists() {
+        fs::create_dir_all(format!("{}\\cover", app_dir.to_string_lossy())).unwrap();
+    }
     let mut doc = EpubDoc::new(path).map_err(|err| err.to_string())?;
     let cover = doc.get_cover();
     match cover {
         Ok(cover) => {
             let title = doc.mdata("title").unwrap();
             // remove disallowed characters from the title string
-            let normalized_title = title
-                .replace("<", "")
-                .replace("\"", "")
-                .replace(":", "")
-                .replace(">", "")
-                .replace("?", "")
-                .replace("|", "")
-                .replace("\\", "")
-                .replace("/", "");
-            let app_dir = app_handle.path_resolver().app_data_dir().unwrap();
+            let normalized_title = normalize_title(&title);
             let cover_path = format!(
                 "{}\\cover\\{}.png",
                 app_dir.to_string_lossy(),
                 normalized_title
             );
-            // println!("{}", cover_path);
+
             let mut cover_file = fs::File::create(cover_path.clone());
             match cover_file {
                 Ok(mut f) => {
                     let _ = f.write_all(&cover);
                 }
                 Err(_) => {
-                    fs::create_dir_all(format!("{}\\cover", app_dir.to_string_lossy())).unwrap();
-                    cover_file = fs::File::create(cover_path);
+                    cover_file = fs::File::create(cover_path.clone());
 
                     match cover_file {
                         Ok(mut f) => {
