149a16ae9e90af30d93554d24d64421a22d4997c
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2024-03-03 23:22:20 +0100

    removed annotations

--- src/routes/book/[book_path]/+page.ts
+++ src/routes/book/[book_path]/+page.ts
@@ -1,9 +1,10 @@
-import type { PageLoad } from './$types';
+import type { PageLoad } from "./$types";
 
 export const load = (async ({ params }) => {
-    return {
-        path: params.book_path,
-    };
+  return {
+    path: params.book_path,
+  };
 }) satisfies PageLoad;
 
 export const prerender = false;
+
